d722314162200c728568346ca47029ad9363b625
Petro <Petro@DESKTOP-AKBD764>
2020-12-31 22:31:46 -0800

    2

--- Bigu_Petronela_Lab7/MainWindow.xaml.cs
+++ Bigu_Petronela_Lab7/MainWindow.xaml.cs
@@ -14,33 +14,45 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.Entity;
 using AutoLotModel;
+using System.Data;
 
 namespace Bigu_Petronela_Lab7
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-     enum ActionState {
-             New,    
-             Edit, 
-             Delete, 
-             Nothing
-        }
-public partial class MainWindow : Window
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+    public partial class MainWindow : Window
     {
         ActionState action = ActionState.Nothing;
-        AutoLotEntitiesModel ctx = new AutoLotEntitiesModel(); 
+        AutoLotEntitiesModel ctx = new AutoLotEntitiesModel();
         CollectionViewSource customerViewSource;
+        Binding custIdTextBoxBinding = new Binding();
+        Binding lastNameTextBoxBinding = new Binding();
+        Binding firstNameTextBoxBinding = new Binding();
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
+            custIdTextBoxBinding.Path = new PropertyPath("CustId");
+            firstNameTextBoxBinding.Path = new PropertyPath("FirstName");
+            lastNameTextBoxBinding.Path = new PropertyPath("LastName");
+            custIdTextBox.SetBinding(TextBox.TextProperty, custIdTextBoxBinding);
+            firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameTextBoxBinding);
+            lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameTextBoxBinding);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource"))); 
-            customerViewSource.Source = ctx.Customers.Local; ctx.Customers.Load();
+            customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            customerViewSource.Source = ctx.Customers.Local;
+            ctx.Customers.Load();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -48,31 +60,128 @@ public partial class MainWindow : Window
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            
-                action = ActionState.New;
-                btnNew.IsEnabled = false;
-                btnEdit.IsEnabled = false;
-                btnDelete.IsEnabled = false;
-
-                btnSave.IsEnabled = true;
-                btnCancel.IsEnabled = true;
-                custIdTextBox.IsEnabled = false;
-                btnPrev.IsEnabled = false;
-                btnNext.IsEnabled = false;
-                firstNameTextBox.IsEnabled = true;
-                lastNameTextBox.IsEnabled = true;
-                BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
-                BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
-                lastNameTextBox.Text = "";
-                firstNameTextBox.Text = "";
-                //Keyboard.Focus(txtPhoneNumber);
+            Customer customer = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    //instantiem phoneNumber entity
+                    customer = new Customer()
+                    {
+                        FirstName = firstNameTextBox.Text.Trim(),
+                        LastName = lastNameTextBox.Text.Trim()
+                    };
+                    //adaugam entitatea nou creata in context
+                    ctx.Customers.Add(customer);
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                btnDelete.IsEnabled = true;
+                custIdTextBox.IsEnabled = true;
+                btnPrev.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                firstNameTextBox.IsEnabled = false;
+                lastNameTextBox.IsEnabled = false;
             }
+            else
+            if (action == ActionState.Edit)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    customer.FirstName = firstNameTextBox.Text.Trim();
+                    customer.LastName = lastNameTextBox.Text.Trim();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+                // pozitionarea pe item-ul curent customerViewSource.View.MoveCurrentTo(customer);
+
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                btnDelete.IsEnabled = true;
+                custIdTextBox.IsEnabled = true;
+                btnPrev.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                firstNameTextBox.IsEnabled = false;
+                lastNameTextBox.IsEnabled = false;
+                firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameTextBoxBinding);
+                lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameTextBoxBinding);
+                custIdTextBox.SetBinding(TextBox.TextProperty, custIdTextBoxBinding);
+            }
+
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    ctx.Customers.Remove(customer);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                custIdTextBox.IsEnabled = true;
+                btnPrev.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                firstNameTextBox.IsEnabled = false;
+                lastNameTextBox.IsEnabled = false;
+                firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameTextBoxBinding);
+                lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameTextBoxBinding);
+            }
+
+
         }
- }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToNext();
+        }
+    }
+}
+ 
+
 
